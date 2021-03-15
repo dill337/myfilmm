98dedbc83ecfeb982a79f0e29639585ba7381e43
dill337 <64544519+dill337@users.noreply.github.com>
2021-03-14 22:18:09 -0400

    Revert "new dist"
    This reverts commit 55bbf8512421d7153ffa3b275eb936aeeb1f08a4.

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -55313,7 +55313,7 @@ var MainView = /*#__PURE__*/function (_React$Component) {
     _this.state = {
       movies: [],
       selectedMovie: null,
-      user: null,
+      user: true,
       registered: null,
       genres: [],
       genreMovies: [],
@@ -55789,7 +55789,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54183" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62016" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
